f506fa37d79138ff18cac57dfe93ab45cf0eb160
Oussama <smahioussama1@gmail.com>
2020-11-20 10:54:01 +0100

    push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <input type>
-    <div class="search-wrapper">
-    <input type="text" v-model="search" placeholder="Search title.."/>
-  </div>
+<div>
     <v-card>
       <v-card-title class="title"> Contracts </v-card-title>
       <v-container>
-        <v-simple-table fixed-header>
+          <v-text-field
+            v-model="search"
+            label="Filter"
+            class="mx-4"
+          ></v-text-field>
+        <v-simple-table fixed-header :custom-filter="filterOnlyCapsText">
           <template v-slot:default>
             <thead>
               <tr>
                 <th class="text-left">Product ID</th>
+                <th class="text-left">Name</th>
                 <th class="text-left">Date</th>
                 <th class="text-left">Status</th>
                 <th class="text-left">Contract</th>
@@ -20,6 +22,7 @@
             <tbody>
               <tr v-for="contract in theContracts" :key="contract.id">
                 <td>{{ contract.productId }}</td>
+                <td>{{ selectedProduct.name}}</td>
                 <td>{{ contract.effectiveDate }}</td>
                 <td>{{ contract.status }}</td>
                 <td>
@@ -39,13 +42,29 @@
 
 <script>
 import contracts from '../assets/data/contracts.json';
+import products from '../assets/data/products.json';
 
 export default {
   name: 'Home',
   data() {
     return {
       theContracts: contracts,
+      theProducts: products,
+      search: '',
     };
+  },
+  computed: {
+    selectedProduct() {
+      const pId = contracts.map((contract) => contract.productId);
+      const selectedProduct = this.theProducts.find((p, index) => p.id === pId[index]);
+      console.log(selectedProduct);
+      return selectedProduct;
+    },
+  },
+  methods: {
+    filter(value, search) {
+      return value != null && search != null && typeof value === 'string';
+    },
   },
 };
 </script>
